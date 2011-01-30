37ac8116de53a7d4133655247059df9d818bcd8d
Christian Theune <ct@flyingcircus.io>
2011-01-30 12:00:31 +0100

    Preparing release 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='asm.cmsui',
-      version='0.1.4dev',
+      version='0.1.4',
       description="Assembly CMS - CMS UI package",
       author="Webcrew",
       author_email="web@assembly.org",
