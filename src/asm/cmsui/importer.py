6c3b350b26c8a66c68b8768b7c091e6a6ba1e0d3
Jussi Judin <462528+Barro@users.noreply.github.com>
2011-02-13 17:39:44 +0200

    Display errors and abort transaction if such things happen on import.

--- src/asm/cmsui/importer.py
+++ src/asm/cmsui/importer.py
@@ -1,11 +1,13 @@
 # Copyright (c) 2010 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import grok
-import asm.cmsui.form
-import asm.cmsui.base
-import zope.interface
 import asm.cms.cms
+import asm.cms.importer
+import asm.cmsui.base
+import asm.cmsui.form
+import grok
+import transaction
+import zope.interface
 
 class ImportActions(grok.Viewlet):
 
@@ -18,7 +20,19 @@ class Import(asm.cmsui.form.Form):
     grok.context(asm.cms.cms.CMS)
     form_fields = grok.AutoFields(asm.cms.interfaces.IImport)
 
+    display_max_errors = 5
+
     @grok.action(u'Import')
     def import_action(self, data):
-        importer = asm.cms.Importer(self.context, data)
-        importer()
+        importer = asm.cms.importer.Importer(self.context, data)
+
+        errors = importer()
+        if len(errors) > 0:
+            transaction.abort()
+
+        if len(errors) > 0:
+            self.flash(u"Aborted import due to following errors:")
+        if len(errors) > self.display_max_errors:
+            self.flash(u"Too many errors detected. Displaying the first %d errors." % self.display_max_errors)
+        for error in errors[:self.display_max_errors]:
+            self.flash(error, "warning")
