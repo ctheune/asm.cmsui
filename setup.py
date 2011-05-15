8d462229737ae47df75523d7629922199c5e50d2
Christian Theune <ct@gocept.com>
2011-05-15 11:28:36 +0300

    Preparing release 0.1.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='asm.cmsui',
-      version='0.1.10dev',
+      version='0.1.10',
       description="Assembly CMS - CMS UI package",
       author="Webcrew",
       author_email="web@assembly.org",
