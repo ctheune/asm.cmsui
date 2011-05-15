98f96c4e79163d0b9c8bf0e10b6605713b1c1581
Jussi Judin <jjudin+asmcms@iki.fi>
2011-05-15 10:23:09 +0300

    Removing extra Javascript search call for the current site.

--- src/asm/cmsui/public/static/search.js
+++ src/asm/cmsui/public/static/search.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $("#search-sites a").each(function(site_index, site) {
         $.getJSON(
-            $(site).attr("href") + "?" + $("#searchform").serialize(),
+            $(site).attr("href") + "/@@search.json?" + $("#searchform").serialize(),
             function(search_results) {
                 $(search_results).each(function(index, element) {
-                    $("#search-results dl").append(
+                    $("#search-results .external-results").append(
                         "<dt><a href='" + element['url'] + "'>" + element["title"] + "</a></dt>\n" +
                             "<dd><div class='preview'>" + element["content"] + "</div></dd>"
                     );
