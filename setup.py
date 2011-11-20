f92a6d17e648108ea8ef18606173267020d30bb7
Christian Theune <ct@flyingcircus.io>
2011-11-20 10:57:52 +0200

    Back to development: 0.1.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='asm.cmsui',
-      version='0.1.17',
+      version='0.1.18dev',
       description="Assembly CMS - CMS UI package",
       author="Webcrew",
       author_email="web@assembly.org",
